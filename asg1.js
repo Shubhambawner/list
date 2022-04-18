29ad9d60aa6ac8f942f36c02600e547e968d9fe1
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-04-18 14:46:21 +0530

    styling

--- asg1.js
+++ asg1.js
@@ -1,8 +1,8 @@
-window.alert(' tapping on any item will select it and display it in selected element box')
-window.alert(' each element has buttons ++ += -')
-window.alert('++ will add one element child in it')
-window.alert('+= will add selected element as child in it')
-window.alert('-  will remove it')
+// window.alert(' tapping on any item will select it and display it in selected element box')
+// window.alert(' each element has buttons ++ += -')
+// window.alert('++ will add one element child in it')
+// window.alert('+= will add selected element as child in it')
+// window.alert('-  will remove it')
 
 let a = 1;
 
@@ -25,6 +25,6 @@ $("body").on("click","#addSelected", function() {
 
 $("body").on("click", "#remove", function(){$(this).parents().first().remove(); someIntentionalBug();})
 
-$("body").on("click", "div#dynamic", function(){$(`div#selected`)[0].innerHTML = `<div>${$(this).html()}</div>`; someIntentionalBug(); })
+$("body").on("click", "div#dynamic", function(){$(`div#selected`)[0].innerHTML = `<div >${$(this).html()}</div>`; someIntentionalBug(); })
 
 $("body").on("click", "input", function(){ someIntentionalBug(); })
